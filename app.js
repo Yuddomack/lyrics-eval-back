da008c0ffd79a7d0bd7af627d726123a79916f11
yuddomack <darr880406@gmail.com>
2020-06-09 14:51:28 +0900

    update: 404 not found 처리

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const createError = require('http-errors');
 const configs = require('./configs');
 
 const app = express();
@@ -6,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', require('./routes'));
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
 
 app.listen(configs.PORT, () => {
   console.log(`[${configs.configStatus}] Connect ${configs.PORT} port`);
