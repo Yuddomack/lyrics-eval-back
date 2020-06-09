bafb54988efae5e73e0130b7ca1d736aaa698da1
yuddomack <darr880406@gmail.com>
2020-06-09 12:02:07 +0900

    update: env를 사용하여 config 세팅

--- configs/index.js
+++ configs/index.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const { env } = require('./args');
 
 const config = {
   dev: {
     configStatus: 'dev',
+    PORT: process.env.DEV_PORT || 9001,
   },
   prod: {
     configStatus: 'prod',
+    PORT: process.env.PROD_PORT || 9001,
   },
 };
 
