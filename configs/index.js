1c65d990eb99424a9a1b738ed0e24e7295820513
yuddomack <darr880406@gmail.com>
2020-06-09 16:04:35 +0900

    update: timeout limit 추가

--- configs/index.js
+++ configs/index.js
@@ -5,10 +5,12 @@ const config = {
   dev: {
     configStatus: 'dev',
     PORT: process.env.DEV_PORT || 9001,
+    TIMEOUT_LIMIT: process.env.DEV_TIMEOUT_LIMIT || '8s',
   },
   prod: {
     configStatus: 'prod',
     PORT: process.env.PROD_PORT || 9001,
+    TIMEOUT_LIMIT: process.env.PROD_TIMEOUT_LIMIT || '8s',
   },
 };
 
