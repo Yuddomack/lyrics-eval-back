d5b8fe3992fa3c40cfb04b356d6f97ff7cd75003
yuddomack <darr880406@gmail.com>
2020-06-09 15:11:46 +0900

    update: error handler 추가

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.locals.message = err.message;
+  res.locals.error = configs.configStatus === 'dev' ? err : {};
+  res.status(err.status || 500);
+  res.send(configs.configStatus === 'dev' ? { err } : { err: err.status });
+});
+
 app.listen(configs.PORT, () => {
   console.log(`[${configs.configStatus}] Connect ${configs.PORT} port`);
 });
