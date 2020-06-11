6e684b713e3ce9e1acc4d023c9ab60279844ccef
yuddomack <darr880406@gmail.com>
2020-06-11 15:38:40 +0900

    update: db connect 정보 추가

--- configs/index.js
+++ configs/index.js
@@ -8,6 +8,9 @@ const config = {
     TIMEOUT_LIMIT: process.env.DEV_TIMEOUT_LIMIT || '8s',
     LOG_FORMAT:
       ':method :url :status :response-time ms - :res[content-length] body - :body',
+    DB_HOST: process.env.DEV_DB_HOST || 'localhost',
+    DB_USER: process.env.DEV_DB_USER || 'forus',
+    DB_PWD: process.env.DEV_DB_PWD || '1111',
   },
   prod: {
     configStatus: 'prod',
@@ -15,6 +18,9 @@ const config = {
     TIMEOUT_LIMIT: process.env.PROD_TIMEOUT_LIMIT || '8s',
     LOG_FORMAT:
       ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" body - :body',
+    DB_HOST: process.env.PROD_DB_HOST || 'localhost',
+    DB_USER: process.env.PROD_DB_USER || 'forus',
+    DB_PWD: process.env.PROD_DB_PWD || '1111',
   },
 };
 
