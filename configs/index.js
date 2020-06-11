056ac17a5078a9bf9da3fa6c6301649524890761
yuddomack <darr880406@gmail.com>
2020-06-11 14:42:52 +0900

    update: morgan log format 추가

--- configs/index.js
+++ configs/index.js
@@ -6,11 +6,15 @@ const config = {
     configStatus: 'dev',
     PORT: process.env.DEV_PORT || 9001,
     TIMEOUT_LIMIT: process.env.DEV_TIMEOUT_LIMIT || '8s',
+    LOG_FORMAT:
+      ':method :url :status :response-time ms - :res[content-length] body - :body',
   },
   prod: {
     configStatus: 'prod',
     PORT: process.env.PROD_PORT || 9001,
     TIMEOUT_LIMIT: process.env.PROD_TIMEOUT_LIMIT || '8s',
+    LOG_FORMAT:
+      ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" body - :body',
   },
 };
 
